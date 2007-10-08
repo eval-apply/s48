bd84969388bc8ddabd7f6eeebecdf8ef0f4f91ea
Mike Sperber <sperber@deinprogramm.de>
2007-10-08 18:55:45 +0200

    Fix division simplifier in PreScheme compiler.
    0 and 1 were swapped.

---
+++ scheme/alt/features-packages.scm
@@ -0,0 +1,29 @@
+; Copyright (c) 1993-2007 by Richard Kelsey and Jonathan Rees. See file COPYING.
+
+
+
+; The following several packages have Scheme-implementation-specific 
+; variants that are much better for one reason or another than
+; the generic versions defined here.
+
+(define-structures ((low-exceptions low-exceptions-interface)
+		    (handle (export ignore-errors))
+		    (features features-interface))
+  (open scheme-level-2)
+  (files features))
+
+(define-structure records records-interface
+  (open scheme-level-2 problems)
+  (files record))
+
+(define-structure ascii (export ascii->char char->ascii)
+  (open scheme-level-2 problems)
+  (files ascii))
+
+(define-structure bitwise bitwise-interface
+  (open scheme-level-2 problems)
+  (files bitwise))
+
+(define-structure code-vectors code-vectors-interface
+  (open scheme-level-1)
+  (files code-vectors))
