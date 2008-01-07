812e1fd67d5fef8c8f314659ccaf5b03756bf315
Marcus Crestani <crestani@informatik.uni-tuebingen.de>
2008-01-07 15:22:21 +0100

    Replace string hash function with string hash function from srfi-13.

---
+++ scheme/vm/heap/image-util.scm
@@ -0,0 +1,13 @@
+; Copyright (c) 1993-2007 by Richard Kelsey and Jonathan Rees. See file COPYING.
+
+(define-enumeration area-type-size
+  (small large weaks))
+
+; at least one of these needs to be synchronized with what the linker writes
+(define-enumeration image-format
+  (two-space bibop)) 
+
+(define (valid-image-format? f)
+  (enum-case image-format f
+    ((two-space bibop) #t)
+    (else #f)))
