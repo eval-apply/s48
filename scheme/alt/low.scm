47da128872bd1a9a9f4066bd8e16c01fa84bf56b
Mike Sperber <sperber@deinprogramm.de>
2008-08-26 13:35:52 +0200

    Unbreak `system'.
    The wrong version was getting exported from the external code.

---
+++ scheme/alt/low.scm
@@ -0,0 +1,12 @@
+; Copyright (c) 1993-2008 by Richard Kelsey and Jonathan Rees. See file COPYING.
+
+
+; Portable versions of low-level things that would really like to rely
+; on the Scheme 48 VM or on special features provided by the byte code
+; compiler.
+
+(define (vector-unassigned? v i) #f)
+
+(define maybe-open-input-file open-input-file)
+(define maybe-open-output-file open-output-file)
+
