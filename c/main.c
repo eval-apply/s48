f92dde8afc78f0c2e7a3fb1d33104e69a1f39e59
Roderic Morris <roderyc@gmail.com>
2009-11-27 01:05:55 -0500

    Add SET-ENVIRONMENT-VARIABLE!.

---
+++ c/main.c
@@ -0,0 +1,15 @@
+
+extern long s48_initialize(int *argc, char ***argv);
+
+int
+main(int argc, char **argv)
+{
+  long return_value;
+
+  return_value = s48_initialize(&argc, &argv);
+
+  if (return_value != 0)
+    return return_value;
+  else
+    return s48_call_startup_procedure(argv, argc);
+}
