4f0660bf57ac488a04ee781a9aa2bf52deed0624
Artem Mironov <art@artsoft.net.ua>
2010-01-20 07:37:22 +0200

    fix the build on Windows with MSC

---
+++ scheme/rts-packages-32.scm
@@ -0,0 +1,10 @@
+; Copyright (c) 1993-2008 by Richard Kelsey and Jonathan Rees. See file COPYING.
+
+
+; Platform-specific constants
+
+(define-structure platform platform-interface
+  ;; don't open anything---this is loaded before even SCHEME-LEVEL-0 is available
+  (define-all-operators)
+  (usual-transforms and cond do let let* or)
+  (files ((vm data) platform-32)))
