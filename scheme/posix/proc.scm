ca245d1f0328e4501bcd5030fa9c75499511699b
Roderic Morris <roderyc@gmail.com>
2011-11-12 14:17:51 -0500

    Fix errors in the posix regepx library related to handling errors.

---
+++ scheme/posix/proc.scm
@@ -0,0 +1,175 @@
+; Copyright (c) 1993-2008 by Richard Kelsey and Jonathan Rees. See file COPYING.
+
+
+; 3.1 Process Creation and Execution
+
+(import-dynamic-externals "=scheme48external/posix")
+;
+; FORK returns the child pid in the parent, and #f in the child.
+
+(define (fork)
+  (force-channel-output-ports!)
+  (posix-fork))
+
+(import-lambda-definition-2 posix-fork ())
+
+; Fork off a process to execute THUNK, but don't return a pid.  This avoids
+; any problem with zombies.
+
+(define (fork-and-forget thunk)
+  (cond ((fork)
+	 => wait-for-child-process)
+	((fork)
+	 (exit 0))
+	(else
+	 (thunk))))
+
+;----------------
+; The base Scheme procedure for exec() is EXEC-WITH-ALIAS (the `alias' is
+; because the first argument may or may not be the name of the program or file).
+;
+; (EXEC-WITH-ALIAS program lookup? environment arguments)
+;   program: byte vector, name of file or program
+;   lookup?: should the program be looked up in PATH?
+;   environment: either #f, which uses the parent's environment in the child,
+;            or a list of byte vectors, representing text of the form "name=value".
+;   arguments: a list of byte vectors
+
+(import-lambda-definition-2 external-exec-with-alias (program lookup? environment arguments)
+			  "posix_exec")
+
+(define (thing->exec-arg-byte-string thing)
+  (x->os-byte-vector thing))
+
+(define (exec-with-alias program lookup? environment arguments)
+  (external-exec-with-alias (thing->exec-arg-byte-string program)
+			    lookup?
+			    (and environment
+				 (map thing->exec-arg-byte-string environment))
+			    (map thing->exec-arg-byte-string arguments)))
+
+; Four versions of exec():
+;  - program looked up, use default environment
+;  - program looked up, environment argument
+;  - file used as-is, use default environment
+;  - file used as-is, environment argument
+;
+; In all cases, the program or file is added to the beginning of the list
+; of arguments.
+;
+; When given, ENV should be a list of strings of the form "name=value".
+
+(define (exec program . arguments)
+  (exec-with-alias program #t #f (cons program arguments)))
+
+(define (exec-with-environment program env . arguments)
+  (exec-with-alias program #t env (cons program arguments)))
+
+(define (exec-file file . arguments)
+  (exec-with-alias file #f #f (cons file arguments)))
+
+(define (exec-file-with-environment file env . arguments)
+  (exec-with-alias file #f env (cons file arguments)))
+
+;----------------
+; Process ids
+;
+; Threads can wait for child process to terminate.  This requires polling,
+; which in turn requires that we store the child's return status or terminating
+; signal for later use.  Hence the extra fields.
+;
+; These should probably not be dumpable.
+;
+; Because these have state they must be unique.  The C code keeps a weak
+; list of existing ones.
+
+(define-record-type process-id :process-id
+  (process-ids-are-made-from-c)
+  process-id?
+  (uid process-id->integer)		; the Unix PID
+  ; The rest are initially #F and are set as events warrant.
+  (exit-status process-id-exit-status)
+  (terminating-signal process-id-terminating-signal)
+  ;; When a thread waits on this process id, it'll add its uid to this queue, and keep a
+  ;; reference. When the event comes in, the event handler will note all the uids in the queue. When
+  ;; each waiting thread wakes up, it'll unregister its uid.
+  (wait-queue process-id-wait-queue set-process-id-wait-queue!))
+
+(define-record-discloser :process-id
+  (lambda (process-id)
+    (list 'process-id (process-id->integer process-id))))
+
+(define (process-id=? p1 p2)
+  (if (and (process-id? p1)
+	   (process-id? p2))
+      (eq? p1 p2)
+      (assertion-violation 'process-id=? "argument type error" p1 p2)))
+
+; We need to make these in the outside world.
+(define-exported-binding "posix-process-id-type" :process-id)
+
+(import-lambda-definition-2 integer->process-id (int) "posix_enter_pid")
+
+;----------------
+; 3.2 Process Termination
+;
+; pid_t wait(int *stat_loc)
+; pid_t waitpid(pid_t pid, int *stat_loc, int options)
+; void _exit(int status);         Need to do this.
+
+; Wait for a child process.  If the child isn't already known to have terminated
+; we process any waiting, terminated children and try again.  If it still hasn't
+; finished we created an external event uid for it and block.
+
+(define (wait-for-child-process pid)
+  (if (not (process-id? pid))
+      (assertion-violation wait-for-child-process "not a process id" pid))
+  (or (process-id-exit-status pid)
+      (process-id-terminating-signal pid)
+      (begin
+        (process-terminated-children pid)
+        (disable-interrupts!)
+        (or (process-id-exit-status pid)
+            (process-id-terminating-signal pid)
+            (really-wait-for-child-process pid))
+        (enable-interrupts!)))
+  (values))
+
+(define (really-wait-for-child-process pid)
+  (let ((wait-queue (or (process-id-wait-queue pid)
+                        (let ((new-queue (make-queue)))
+                          (set-process-id-wait-queue! pid new-queue)
+                          new-queue)))
+        (wait-uid (posix-create-wait-uid)))
+    (enqueue! wait-queue wait-uid)
+    (wait-for-external-event wait-uid)
+    (posix-unregister-wait-uid wait-uid)))
+
+; Waiting for children.  We go through the terminated child processes until we
+; find the one we are looking for or we run out.  This needs to be called by
+; the SIGCHLD handler.
+
+(define (process-terminated-children . maybe-pid)
+  (let ((pid (if (null? maybe-pid)
+                 #f
+                 (car maybe-pid))))
+    (let loop ()
+      (let ((next (posix-waitpid)))
+        (if next
+            (let ((wait-queue (process-id-wait-queue next)))
+              (if wait-queue
+                  (begin
+                    (for-each (lambda (x) (posix-note-wait-uid x))
+                              (queue->list wait-queue))
+                    (set-process-id-wait-queue! next #f)))
+              (if (not (eq? pid next))
+                  (loop))))))))
+
+(import-lambda-definition-2 posix-waitpid ())
+(import-lambda-definition-2 posix-create-wait-uid ())
+(import-lambda-definition-2 posix-note-wait-uid (wait-uid))
+(import-lambda-definition-2 posix-unregister-wait-uid (wait-uid))
+
+(define (exit status)
+  (force-channel-output-ports!)
+  (scheme-exit-now status))
