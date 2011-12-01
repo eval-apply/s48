dbece92080d22220c802dee562c1cb73ac5bbc5d
Will Noble <will@cow9.org>
2011-11-30 18:17:00 -0800

    Include macro eval in the cenv made by BIND-ALIASES.
    Was included before the cenv representation switch. Not including it causes problems.

---
+++ c/posix/posix.h
@@ -0,0 +1,10 @@
+
+extern mode_t		s48_extract_mode(s48_call_t call, s48_ref_t sch_mode);
+extern int		s48_extract_file_options(s48_call_t call, s48_ref_t sch_file_options);
+
+extern s48_ref_t	s48_enter_uid(s48_call_t call, uid_t uid);
+extern uid_t		s48_extract_uid(s48_call_t call, s48_ref_t uid);
+
+extern s48_ref_t	s48_enter_gid(s48_call_t call, gid_t gid);
+extern gid_t		s48_extract_gid(s48_call_t call, s48_ref_t gid);
+
